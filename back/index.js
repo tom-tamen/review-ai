e83b6ff03424d9420be7179a9622ed8f401685df
Charles <charles.chrismann@free.fr>
2024-06-12 15:09:26 +0200

    add reviews

--- back/index.js
+++ back/index.js
@@ -1,6 +1,18 @@
 import 'dotenv/config';
 import MistralClient from '@mistralai/mistralai';
 import express from 'express';
+import Sentiment from 'sentiment';
+import * as fs from 'fs'
+import csv from 'csv-parser'
+
+const sentiment = new Sentiment()
+let results = []
+
+fs.createReadStream('netflix_reviews.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+  });
 
 const app = express();
 app.use(express.json());
@@ -11,17 +23,25 @@ const apiKey = process.env.MISTRALAI_API_KEY;
 
 const client = new MistralClient(apiKey);
 
-app.get('/review', async (req, res) => {
-    const shop = req.body.shop;
-    const chatResponse = await client.chat({
-        model: 'mistral-tiny',
-        messages: [
-            {role: 'system', content: "Generate a list of 50 random reviews in french for a shop in a JavaScript array format. Each review should be structured as an object within the array, containing two properties: 'author' and 'content'. 'author' should be a randomly generated name, and 'content' should contain a review that balances both positive and negative aspects with a random sentiment ratio. The reviews should realistically simulate what might be found in customer feedback, with a natural mix of satisfaction and critique. The final array should be formatted as follows: [{author: 'Name', content: 'Review text'}, ...]."},
-            {role: 'user', content: `My shop is "${shop}"`}
-        ],
-    });
-
-    res.status(200).send(chatResponse.choices[0].message.content);
+app.get('/api/reviews', async (req, res) => {
+  const itemCount = Math.random() * 1000
+  const resp = []
+  for(let i = 0; i < itemCount; i++) {
+    const item = results[Math.floor(Math.random()*results.length)];
+    const a = sentiment.analyze(item.content)
+    const s = {
+      sentiment: {
+        score: a.score,
+        comparative: a.comparative,
+        positive: a.positive,
+        negative: a.negative
+      },
+      content: item.content,
+      rating: item.score
+    }
+    resp.push(s)
+  }
+    res.send(resp);
 });
 
 // Start the server
